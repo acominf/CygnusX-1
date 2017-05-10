9bd42c3fcaf36e31204bb16279331723695b56a5
Morcado <gposcarr18@hotmail.com>
2017-05-10 14:46:59 -0500

    aaaa

---
+++ core/src/com/cygnusx1/game/Botiquin.java
@@ -0,0 +1,10 @@
+package com.cygnusx1.game;
+
+/**
+ * Created by Oscar Patiño on 04/05/2017.
+ */
+public class Botiquin {
+    public Botiquin(){
+        
+    }
+}
---
+++ html/src/com/cygnusx1/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.cygnusx1.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.cygnusx1.game.CygnusX1;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CygnusX1();
+        }
+}
